a5c7970d823066178738598d12b0a0cbba18feeb
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-05-25 15:29:35 +0500

    Created react project,

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <div class="w-full bg-[#014976] text-white h-10 flex items-center justify-between">
+        <div></div>
+        <div>
+          <a href="">Home</a>
+        </div>
+      </div>
+    </>
+  )
+}
