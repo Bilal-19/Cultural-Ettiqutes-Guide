a68390696e92086ce3cea752e57729a88fe6d4af
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-05-26 16:29:18 +0500

    Added blog records and updated home page.

--- src/Data/blogs.js
+++ src/Data/blogs.js
@@ -1,18 +1,48 @@
 const blogRecords = [
     {
         id: 1,
-        thumbnailPath: "./JapanBlog.png",
-        blogTitle: "5 things you should never do in Japan"
+        thumbnailPath: "https://images.pexels.com/photos/1428347/pexels-photo-1428347.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        blogTitle: "10 Common Cultural Mistakes Travelers Make (And How to Avoid Them)"
     },
     {
         id: 2,
-        thumbnailPath: "./JapanBlog.png",
-        blogTitle: "5 things you should never do in UAE"
+        thumbnailPath: "https://images.pexels.com/photos/8112177/pexels-photo-8112177.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        blogTitle: "How to Say Hello Around the World"
     },
     {
         id: 3,
-        thumbnailPath: "./JapanBlog.png",
-        blogTitle: "5 things you should never do in Pakistan"
+        thumbnailPath: "https://images.pexels.com/photos/6950747/pexels-photo-6950747.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        blogTitle: "Dining Etiquette from 12 Countries"
+    },
+    {
+        id: 4,
+        thumbnailPath: "https://images.pexels.com/photos/2577274/pexels-photo-2577274.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        blogTitle: "Cultural Faux Pas in Japan, France, and Pakistan"
+    },
+    {
+        id: 5,
+        thumbnailPath: "https://images.pexels.com/photos/585292/pexels-photo-585292.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        blogTitle: "Why Tipping in Some Countries Is Rude"
+    },
+    {
+        id: 6,
+        thumbnailPath: "https://images.unsplash.com/photo-1667960733385-4ae873550943?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        blogTitle: "Dress Codes Around the World"
+    },
+    {
+        id: 7,
+        thumbnailPath: "https://images.pexels.com/photos/32239774/pexels-photo-32239774/free-photo-of-stunning-interior-of-mosque-in-istanbul-turkiye.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        blogTitle: "What to Wear to Religious Sites"
+    },
+    {
+        id: 8,
+        thumbnailPath: "https://images.unsplash.com/photo-1728290838813-9c94d553d988?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        blogTitle: "How to Behave as a Guest in a Foreign Home"
+    },
+    {
+        id: 9,
+        thumbnailPath: "https://images.pexels.com/photos/32213237/pexels-photo-32213237/free-photo-of-group-of-young-adults-engaging-with-a-tablet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        blogTitle: "How Etiquette Builds Connections"
     }
 ]
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ import Header from "../Components/header";
 import { Link } from "react-router-dom";
 
 const fetchCountries = countryRecords.slice(0, 5);
+const fetchBlogs = blogRecords.slice(0,3);
 
 export default function Home() {
     return (
@@ -24,11 +25,11 @@ export default function Home() {
                 Featured Blogs
             </h2>
             <div class="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-10 pb-15 md:pb-30">
-                {blogRecords.map((val, key) =>
+                {fetchBlogs.map((val, key) =>
                     <>
                         <div className="mx-auto w-80 md:w-auto">
-                            <img src={val.thumbnailPath} alt={val.blogTitle} />
-                            <p className="text-md md:text-lg">{val.blogTitle}</p>
+                            <img src={val.thumbnailPath} alt={val.blogTitle} className="rounded-lg"/>
+                            <p className="text-md md:text-lg font-medium">{val.blogTitle}</p>
                             <a href="" className="font-light">Read more</a>
                         </div>
                     </>
