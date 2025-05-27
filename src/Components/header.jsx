1205e4d73e02ca8b204429990d0980ad3db186fc
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-05-27 16:16:50 +0500

    Upgrade header toggle functionality.

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,13 +1,12 @@
-import { NavLink,Link } from "react-router-dom";
-
-function toggleMenu() {
-    const toggleBtn = document.getElementById("toggle");
-    toggleBtn.addEventListener("click", function () {
-        document.getElementById("menuOptions").classList.toggle("hidden");
-    })
-}
+import { useState } from "react";
+import { NavLink, Link } from "react-router-dom";
 
 export default function Header() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen)
+    }
     return (
         <>
             <div className="w-full bg-[#014976] text-white py-2 md:h-15 flex flex-col md:flex-row md:items-center justify-between">
@@ -15,11 +14,11 @@ export default function Header() {
                     <Link to="/">
                         <p className="ml-2"><i class="fa-solid fa-globe"></i> Cultural Ettiqutes Guide</p>
                     </Link>
-                    <button id="toggle" className="block md:hidden mr-5 hover:cursor-pointer" onClick={() => toggleMenu()}>
-                        <i class="fa-solid fa-sliders"></i>
+                    <button className="block md:hidden mr-5 hover:cursor-pointer" onClick={toggleMenu}>
+                        <i class="fa-solid fa-bars"></i>
                     </button>
                 </div>
-                <div id="menuOptions" className="space-x-5 flex flex-col md:flex-row items-start hidden md:block space-y-2 md:space-y-0">
+                <div id="menuOptions" className={`space-x-5 flex flex-col md:flex-row items-start space-y-2 md:space-y-0 ${isMenuOpen ? 'block' : 'hidden'} md:flex`}>
                     <NavLink to="/" className="hover:text-[#00FBE6] ml-2 md:ml-0">Home</NavLink>
                     <NavLink to="/countries" className="hover:text-[#00FBE6] ml-2 md:ml-0">Countries</NavLink>
                     <NavLink to="/blogs" className="hover:text-[#00FBE6] ml-2 md:ml-0">Blogs</NavLink>
