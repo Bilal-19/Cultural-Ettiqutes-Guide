d5ef8e083ec7a35558fde7c50e48671a7e88ded2
Bilal-19 <bilalmuhammadyousuf543@gmail.com>
2025-05-27 16:10:43 +0500

    Make newsletter functional.

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,20 +1,48 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
-
+import { toast, ToastContainer } from "react-toastify";
 
 export default function Footer() {
+    const [newsletterForm, setNewsletterForm] = useState({ fname: '', email: '' })
+    const handleChange = (e) => {
+        setNewsletterForm({ ...newsletterForm, [e.target.name]: e.target.value })
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        try {
+            const response = await fetch("https://formsubmit.co/ajax/bilalmuhammadyousuf543@gmail.com", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                },
+                body: JSON.stringify(newsletterForm)
+            });
+
+            if (response.ok) {
+                toast.success("Thankyou for subscribing our newsletter.");
+                setNewsletterForm({ fname: '', email: '' })
+            } else {
+
+            }
+        } catch (err) {
+            toast.error("Network error. Please try again later.")
+        }
+    };
     return (
         <>
             <div class="w-full bg-[#014976] text-white flex flex-col md:flex-row justify-around p-5 md:p-10 space-y-5 md:space-y-0">
                 <div>
                     <h4 className="font-bold text-xl md:text-2xl">Follow Us On Social Media</h4>
                     <div className="text-md md:text-2xl flex flex-row space-x-3">
-                        <a href="">
+                        <a href="#">
                             <i class="fa-brands fa-facebook"></i>
                         </a>
-                        <a href="">
+                        <a href="#">
                             <i class="fa-brands fa-instagram"></i>
                         </a>
-                        <a href="">
+                        <a href="#">
                             <i class="fa-brands fa-linkedin"></i>
                         </a>
                     </div>
@@ -33,11 +61,12 @@ export default function Footer() {
                 <div>
                     <h4 className="font-bold text-xl md:text-2xl">Newsletter</h4>
                     <p className="font-light text-md mb-2">Stay up to date with our latest news</p>
-                    <form action="" className="space-y-2">
-                        <input type="text" className="bg-white rounded-md text-black px-3 py-1 block focus:outline-none" placeholder="Enter your first name" />
-                        <input type="email" className="bg-white rounded-md text-black px-3 py-1 block focus:outline-none" placeholder="Enter your email" />
+                    <form onSubmit={handleSubmit} className="space-y-2">
+                        <input required type="text" className="bg-white rounded-md text-black px-3 py-1 block focus:outline-none" placeholder="Enter your first name" name="fname" value={newsletterForm.fname} onChange={handleChange} />
+                        <input required type="email" className="bg-white rounded-md text-black px-3 py-1 block focus:outline-none" placeholder="Enter your email" name="email" value={newsletterForm.email} onChange={handleChange} />
                         <button className="bg-black text-white rounded-md px-3 py-1 hover:cursor-pointer"><i class="fa-solid fa-bell"></i> Subscribe</button>
                     </form>
+                    <ToastContainer />
                 </div>
             </div>
         </>
